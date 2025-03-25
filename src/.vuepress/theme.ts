77b1f1c39f04dc169f3f246262c47e0623f86186
“flylmt” <1903588250@qq.com>
2025-03-25 22:17:49 +0800

    初步修改

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -150,11 +150,6 @@ displayFooter: true,
       prefix: "fa6-solid:",
     },
     
-    search: {
-      isSearchable: (page) => page.path !== "/",
-      maxSuggestions: 10,
-    },
-
     // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
     // pwa: {
     //   favicon: "/favicon.ico",
